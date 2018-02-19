02d58f3b9f096b331fba51a104afbb399191bc90
David Langford <langfod@gmail.com>
2018-02-19 00:04:54 -0500

    initial commit

---
+++ src/main/java/com/dihelix/langfod/reviewssite/ReviewsSiteApplication.java
@@ -0,0 +1,12 @@
+package com.dihelix.langfod.reviewssite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsSiteApplication.class, args);
+	}
+}
